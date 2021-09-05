0da095858d338b5e96d299319d3653dfa9f7b758
KopylovVadik <kopylov.ddut@gmail.com>
2021-09-05 19:01:52 +0300

    task3 for anresh

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        DataObject dataObject = new DataObject();
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            if (id > 3 || id < 1)
+            {
+                return BadRequest();
+            }
+
+            IValidator CurrentData = dataObject.DataMap[id];
+            return Ok(System.IO.File.ReadAllLines(CurrentData.path));
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, [FromBody] JsonElement body)
+        {
+            if (id > 3 || id < 1)
+            {
+                return BadRequest();
+            }
+
+            string text = body.GetProperty("value").ToString();
+            IValidator CurrentData = dataObject.DataMap[id];
+
+            if (CurrentData.IsValid(text) == true)
+            {
+                System.IO.File.WriteAllText(CurrentData.path, text);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            return Ok(text);
+        }
+    }
+
+
+    public interface IValidator
+    {
+        bool IsValid(string text);
+        string path { get; }
+    }
+
+
+    public class DataObject
+    {
+        public Dictionary<int, IValidator> DataMap = new Dictionary<int, IValidator>
+        {
+            {1, new OnlyRussian()},
+            {2, new OnlyEnglish()},
+            {3, new OnlyNubers()}
+        };
+    }
+
+
+    public class OnlyNubers : IValidator
+    {
+        public string path = @"C:\Users\kopyl\Desktop\Новый текстовый документ3.txt";
+
+        string IValidator.path => path;
+
+
+        public bool IsValid(string text)
+        {
+            Regex regex = new Regex(@"[\d]*");
+            MatchCollection match = regex.Matches(text);
+
+
+            if (text.Length == match[0].Length)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class OnlyRussian : IValidator
+    {
+        public string path = @"C:\Users\kopyl\Desktop\Новый текстовый документ1.txt";
+
+        string IValidator.path => path;
+
+        public bool IsValid(string text)
+        {
+            Regex regex = new Regex(@"[а-я]*", RegexOptions.IgnoreCase);
+            MatchCollection match = regex.Matches(text);
+
+
+            if (text.Length == match[0].Length)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class OnlyEnglish : IValidator
+    {
+        public string path = @"C:\Users\kopyl\Desktop\Новый текстовый документ2.txt";
+
+        string IValidator.path => path;
+
+        public bool IsValid(string text)
+        {
+            Regex regex = new Regex(@"[a-z]*", RegexOptions.IgnoreCase);
+            MatchCollection match = regex.Matches(text);
+
+
+            if (text.Length == match[0].Length)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WebApplication1/wwwroot/js/JavaScript.js
@@ -0,0 +1,72 @@
+﻿
+// Получение данных из файла
+async function GetData() {
+    // отправляет запрос и получаем ответ
+    const response = await fetch("/api/values/" + id,
+        {
+            method: "GET",
+            headers: { "Accept": "application/json" }
+        });
+
+    // если запрос прошел нормально
+    if (response.ok === true) {
+        // получаем данные
+        const data = await response.json();
+        //console.log(data)
+        const filtered_data = data.filter((item) => {
+            return item !== '';
+        });
+
+        //console.log(filtered_data);
+        document.querySelector('#w3review').value = filtered_data;
+    }
+
+}
+
+
+// Редактирование данных из файла
+async function EditData(value) {
+    const response = await fetch("api/values/" + id,
+        {
+            method: "PUT",
+            headers: { "Accept": "application/json", "Content-Type": "application/json" },
+            body: JSON.stringify({
+                value
+            })
+
+        });
+    if (response.ok === true) {
+        const result = await response.json();
+        console.log(result);
+
+        //reset();
+    } else {
+        alert("incorrect data");
+    }
+}
+
+
+let save_button = document.querySelector('#submit')
+save_button.addEventListener('click',
+    (e) => {
+        e.preventDefault()
+        const value = document.querySelector('#w3review').value
+        // логика обработки отправки
+        //console.log(value.split(","))
+        EditData(value);
+
+    })
+
+
+let update_button = document.querySelector('#reset')
+update_button.addEventListener('click',
+    (e) => {
+        e.preventDefault()
+
+        GetData(id);
+
+    })
+
+const id = 3;
+// вызов функции
+GetData();
